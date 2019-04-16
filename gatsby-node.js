0269f1585fc86293d92feff72deb7288cc48d18f
cemujax <cemujax@gmail.com>
2019-04-16 22:35:41 +0900

    ENH:  Add tags page, tag page

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,10 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
-  const blogPost = path.resolve(`./src/templates/post.js`)
+  const postTemplate = path.resolve(`./src/templates/post.js`)
+  const tagPage = path.resolve(`./src/pages/tags.js`)
+  const tagPosts = path.resolve(`./src/templates/tag.js`)
+
   const result = await graphql(
     `
       {
@@ -14,9 +17,11 @@ exports.createPages = async ({ graphql, actions }) => {
         ) {
           edges {
             node {
+              id
               fields {
                 slug
               }
+              excerpt(pruneLength: 160)
               frontmatter {
                 title
                 date
@@ -33,14 +38,52 @@ exports.createPages = async ({ graphql, actions }) => {
     throw result.errors
   }
   const posts = result.data.allMarkdownRemark.edges
+  const postsByTag = {}
 
+  posts.forEach(({ node }) => {
+    if (node.frontmatter.tags) {
+      node.frontmatter.tags.forEach(tag => {
+        if (!postsByTag[tag]) {
+          postsByTag[tag] = []
+        }
+        postsByTag[tag].push(node)
+      })
+    }
+  })
+
+  const tags = Object.keys(postsByTag)
+
+  // create tags page
+  createPage({
+    path: '/tags',
+    component: tagPage,
+    context: {
+      tags: tags.sort(),
+    },
+  })
+
+  //create tag page ex) /tags/exTag
+  tags.forEach(tagName => {
+    const posts = postsByTag[tagName]
+
+    createPage({
+      path: `/tags/${tagName}`,
+      component: tagPosts,
+      context: {
+        posts,
+        tagName,
+      },
+    })
+  })
+
+  // create posts page
   posts.forEach((post, index) => {
     const previous = index === posts.length - 1 ? null : posts[index + 1].node
     const next = index === 0 ? null : posts[index - 1].node
     const slug = post.node.fields.slug
     createPage({
       path: slug,
-      component: blogPost,
+      component: postTemplate,
       context: {
         pathSlug: slug,
         previous,
