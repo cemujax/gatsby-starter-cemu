e4ed7c11d827d90152ae754db426e26222f1b3fd
cemujax <cemujax@gmail.com>
2019-04-16 22:33:16 +0900

    ENH: Added category attribute in PostList

--- src/components/post/PostList/PostList.js
+++ src/components/post/PostList/PostList.js
@@ -3,31 +3,38 @@ import { Link } from 'gatsby'
 
 import './PostList.scss'
 
-const PostItem = ({ id, path, title, body, date, tags }) => {
+const PostItem = ({ id, path, title, body, date, category }) => {
   return (
     <div className="post-item" key={id}>
       <Link to={path}>
         <h2>{title}</h2>
         <p className="date">{date}</p>
         <p>{body}</p>
+        <p>
+          <span className="category">{category}</span>
+        </p>
       </Link>
-      <div className="tags">{tags.join(', ')}</div>
     </div>
   )
 }
 
 const PostList = ({ posts }) => {
   const postList = posts.map(post => {
-    const { id, excerpt: body } = post.node
-    const { path, title, date, tags } = post.node.frontmatter
+    const {
+      id,
+      excerpt: body,
+      fields: { slug },
+    } = post.node
+    const { title, date, category, tags } = post.node.frontmatter
     return (
       <PostItem
         id={id}
         key={id}
-        path={path}
+        path={slug}
         title={title}
         body={body}
         date={date}
+        category={category}
         tags={tags}
       />
     )
