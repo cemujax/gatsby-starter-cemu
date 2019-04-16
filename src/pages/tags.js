eeae58bb21e45b5327f0d26f12c2ccb008af55c5
cemujax <cemujax@gmail.com>
2019-04-17 01:21:40 +0900

    ENH: Add tags page , tag template

---
+++ src/pages/tags.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import SEO from '../components/seo'
+import { Layout } from '../layout'
+import TagList from '../components/tag/TagList'
+
+const TagsPage = ({ pageContext }) => {
+  const { tags } = pageContext
+  return (
+    <Layout>
+      <SEO title="tags" keywords={tags} />
+      <h2>All Tags</h2>
+      <TagList tags={tags} />
+    </Layout>
+  )
+}
+
+export default TagsPage
---
+++ src/templates/tag.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { Layout } from '../layout'
+import PostList from '../components/post/PostList'
+
+const Tag = ({ pageContext }) => {
+  const { tagName } = pageContext
+  const posts = pageContext.posts.map(post => {
+    const obj = {
+      node: post,
+    }
+    return obj
+  })
+  return (
+    <Layout>
+      <div>
+        <div>
+          Tag:{`${tagName}`} - {posts.length} posts
+        </div>
+        <div>
+          <PostList posts={posts} />
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default Tag
