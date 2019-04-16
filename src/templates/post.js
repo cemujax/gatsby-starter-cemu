cf4050f39780f6d502ce9e809975884c6ac8e957
cemujax <cemujax@gmail.com>
2019-04-17 01:28:41 +0900

    CLN: Apply TagList to post template

--- src/templates/post.js
+++ src/templates/post.js
@@ -2,28 +2,34 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 import { Layout } from '../layout'
+import TagList from '../components/tag/TagList'
 
 const Post = ({ data }) => {
   const post = data.markdownRemark
-  const title = post.frontmatter.title
-  const date = post.frontmatter.date
   const html = post.html
+  const { title, date, tags } = post.frontmatter
   return (
     <Layout>
       <h1>{title}</h1>
       <p>{date}</p>
       <div dangerouslySetInnerHTML={{ __html: html }} />
+
+      <TagList tags={tags || []} />
     </Layout>
   )
 }
 
 export const query = graphql`
   query($pathSlug: String!) {
-    markdownRemark(frontmatter: { path: { eq: $pathSlug } }) {
+    markdownRemark(fields: { slug: { eq: $pathSlug } }) {
+      id
       html
+      excerpt(pruneLength: 160)
       frontmatter {
-        date
         title
+        date(formatString: "YYYY-MM-DD HH:MM")
+        category
+        tags
       }
     }
   }
