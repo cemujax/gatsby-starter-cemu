7a0e64efd317f3000d1039f11d7955ce7165638b
cemujax <cemujax@gmail.com>
2019-04-19 01:51:54 +0900

    CLN: siteMetadata

---
+++ site-meta-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: `Starter-cemu`,
+  description: `Gatsby Starter Theme`,
+  author: `cemujax`,
+  siteUrl: `https://gatsby-starter-cemu.netlify.com`,
+  comments: {
+    disqusShortname: `gatsby-starter-cemu`,
+  },
+  github: `https://github.com/cemujax`,
+}
