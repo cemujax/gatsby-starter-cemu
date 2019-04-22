704f3b8f4fb86fd84effc3f0f41f1b4aa7dbc655
cemujax <cemujax@gmail.com>
2019-04-23 05:20:42 +0900

    Style: Post Navigator style

--- src/components/post/PostNavigator/PostNavigator.js
+++ src/components/post/PostNavigator/PostNavigator.js
@@ -11,14 +11,23 @@ const PostNavigator = ({ pageContext }) => {
       <ul>
         <li>
           {previous && (
-            <Link to={previous.fields.slug}>
+            <Link
+              to={previous.fields.slug}
+              className="navigator-btn btn-outline-fill"
+            >
               ← {previous.frontmatter.title}
             </Link>
           )}
         </li>
         <li>
           {next && (
-            <Link to={next.fields.slug}> {next.frontmatter.title} →</Link>
+            <Link
+              to={next.fields.slug}
+              className="navigator-btn btn-outline-fill"
+            >
+              {' '}
+              {next.frontmatter.title} →
+            </Link>
           )}
         </li>
       </ul>
